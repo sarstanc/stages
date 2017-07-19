bb8436fc92fb02575f2003c6a9d1834a9be128bc
Sarah Stancik <skstancik@gmail.com>
2017-07-19 10:33:22 -0400

    start test material

--- js/materials.js
+++ js/materials.js
@@ -1,6 +1,22 @@
-AFRAME.registerShader('custom', {
+AFRAME.registerShader('line-dashed', {
   schema: {
-    emissive: {default: '#000'},
-    wireframe: {default: false}
+    dashSize: {default: 3},
+    lineWidth: {default: 1}
+  },
+
+  /**
+  * `init` used to initialize material. Called once.
+  */
+  init: function (data) {
+    this.material = new THREE.LineDashedMaterial(data):
+    this.update(data); // `update()` currently not called after `init`. (#1834)
+  },
+
+  /**
+  * `update` used to update the material. Called on initialization and when data updates.
+  */
+  update: function (data) {
+    this.material.dashsize = data.dashsize;
+    this.material.lineWidth = data.linewidth;
   }
 });
