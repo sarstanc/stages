8a318652f690f0f2636ed6b6206f837e6b0814e2
Sarah Stancik <skstancik@gmail.com>
2017-07-19 11:41:05 -0400

    test js link

--- js/materials.js
+++ js/materials.js
@@ -3,20 +3,18 @@ AFRAME.registerShader('line-dashed', {
     dashSize: {default: 3},
     lineWidth: {default: 1}
   },
-
   /**
-  * `init` used to initialize material. Called once.
-  */
+   * `init` used to initialize material. Called once.
+   */
   init: function (data) {
-    this.material = new THREE.LineDashedMaterial(data):
-    this.update(data); // `update()` currently not called after `init`. (#1834)
+    this.material = new THREE.LineDashedMaterial(data);
+    this.update(data);  // `update()` currently not called after `init`. (#1834)
   },
-
   /**
-  * `update` used to update the material. Called on initialization and when data updates.
-  */
+   * `update` used to update the material. Called on initialization and when data updates.
+   */
   update: function (data) {
     this.material.dashsize = data.dashsize;
-    this.material.lineWidth = data.linewidth;
+    this.material.linewidth = data.linewidth;
   }
 });
